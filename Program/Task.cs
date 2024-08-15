b16b87ea15c863732f8aae3b2fdf21b78c7d2d57
Naufal Satya Pradananto <146948146+NFLSTY@users.noreply.github.com>
2024-08-15 07:09:21 +0700

    Update Task.cs
    Possible null reference, need to add message

--- Program/Task.cs
+++ Program/Task.cs
@@ -11,8 +11,10 @@ namespace OfficeAutomationSystems
             Task task = new Task();
             Console.Write("Enter Task Name: ");
             task.TaskName = Console.ReadLine();
+            
             Console.Write("Enter Assigned To: ");
             task.AssignedTo = Console.ReadLine();
+            
             Console.Write("Enter Details: ");
             task.Details = Console.ReadLine();
             return task;
